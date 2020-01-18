16740a3918a666f259a67ba74818ea53364cb19a
Ekaterina Blagoeva <erblagoeva@gmail.com>
2020-01-17 21:24:01 -0500

    trying to combine files,i have too many

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    //questions asked
+    var questions = [
+        {
+            question:"1. What are variables used for in JavaScript Programs?",
+            options:["A. Storing numbers, dates, or other values","B. Varying Randomly","C. Causing high-school algebra flashbacks","D. None of the above"],
+            answer:"A. Storing numbers, dates, or other values"
+        },
+        {
+            question:"2. Inside which HTML element do we put the JavaScript?",
+            options:["A. <js>","B. <scripting>", "C. <script>", "D. <javascript>"],
+            answer:"C. <script>"
+        },
+        {
+            question:"3. Which of the following is not considered as an error in JavaScript?",
+            options:["A. Syntax error", "B. Missing of semicolons", "C. Division by zero", "D. All of the mentioned"],
+            answer:"C. Division by zero"
+        },
+        {
+            question:"4. Which built-in method removes the last element from an array and returns that element?",
+            options:["A. last()", "B. get()","C. pop()","D. None of the above"],
+            answer:"C. pop()"
+        },
+        {
+            question:"5. Arrays in JavaScript can be used to store:",
+            choices: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
+            answer: "D. All of the above"
+        }
+    ];
+
+
+
+
+})
